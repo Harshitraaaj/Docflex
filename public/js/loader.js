9b5c27be95f00603d429575c48ceb5087533e3b7
Harshit raj <119807388+Harshitraaaj@users.noreply.github.com>
2025-05-04 14:22:35 +0530

    fixing bugs

--- public/js/loader.js
+++ public/js/loader.js
@@ -1,11 +1,18 @@
 
   const form = document.getElementById('conversionForm');
   const loader = document.getElementById('loader');
+
+  if (form) {
   const submitBtn = form.querySelector('button[type="submit"]');
+
+
 
   form.addEventListener('submit', function () {
     loader.style.display = 'block';      // Show loader
     submitBtn.disabled = true;           // Disable button
+  
    
   });
+
+}
 
